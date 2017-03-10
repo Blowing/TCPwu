bd773fc11744e1e05310d06f565336ff8e63dbd8
wujie <wujie@tenda.cn>
2017-03-10 17:42:59 +0800

    1 修改服务器端代码

--- src/com/wujie/server/ServerWindow.java
+++ src/com/wujie/server/ServerWindow.java
@@ -5,17 +5,12 @@ import java.awt.Frame;
 import java.awt.Label;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 
 
 public class ServerWindow extends Frame {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
     private FileServer s = new FileServer(12345);
     private Label label;
 
@@ -23,70 +18,59 @@ public class ServerWindow extends Frame {
         super(title);
         label = new Label();
         add(label, BorderLayout.PAGE_START);
-        label.setText("服务器端已经启动");
+        label.setText("服务器已经启动");
         this.addWindowListener(new WindowListener() {
-
             @Override
-            public void windowOpened(WindowEvent arg0) {
-                // TODO Auto-generated method stub
+            public void windowOpened(WindowEvent e) {
                 new Thread(new Runnable() {
-
                     @Override
                     public void run() {
-                        // TODO Auto-generated method stub
                         try {
                             s.start();
                         } catch (Exception e) {
-                            // TODO: handle exception
+                            // e.printStackTrace();
                         }
                     }
                 }).start();
             }
 
             @Override
-            public void windowIconified(WindowEvent arg0) {
-                // TODO Auto-generated method stub
-
+            public void windowIconified(WindowEvent e) {
             }
 
             @Override
-            public void windowDeiconified(WindowEvent arg0) {
-                // TODO Auto-generated method stub
-
+            public void windowDeiconified(WindowEvent e) {
             }
 
             @Override
-            public void windowDeactivated(WindowEvent arg0) {
-                // TODO Auto-generated method stub
-
+            public void windowDeactivated(WindowEvent e) {
             }
 
             @Override
-            public void windowClosing(WindowEvent arg0) {
-                // TODO Auto-generated method stub
+            public void windowClosing(WindowEvent e) {
                 s.quit();
                 System.exit(0);
             }
 
             @Override
-            public void windowClosed(WindowEvent arg0) {
-                // TODO Auto-generated method stub
-
+            public void windowClosed(WindowEvent e) {
             }
 
             @Override
-            public void windowActivated(WindowEvent arg0) {
-                // TODO Auto-generated method stub
-
+            public void windowActivated(WindowEvent e) {
             }
         });
     }
-    public static void main(String[] args) throws UnknownHostException {
-        // TODO Auto-generated method stub
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
         InetAddress address = InetAddress.getLocalHost();
         ServerWindow window = new ServerWindow("文件上传服务端：" + address.getHostAddress());
         window.setSize(400, 300);
         window.setVisible(true);
+
     }
 
 }
--- src/com/wujie/server/StreamTool.java
+++ src/com/wujie/server/StreamTool.java
@@ -9,27 +9,25 @@ import java.io.PushbackInputStream;
 
 public class StreamTool {
 
-    public static void save(File file, byte[] data) throws IOException {
+    public static void save(File file, byte[] data) throws Exception {
         FileOutputStream outStream = new FileOutputStream(file);
         outStream.write(data);
         outStream.close();
     }
 
-    public static String reaLine(PushbackInputStream in) throws IOException  {
+    public static String readLine(PushbackInputStream in) throws IOException {
         char buf[] = new char[128];
         int room = buf.length;
         int offset = 0;
         int c;
-        loop:   while(true) {
-            switch ( c = in.read()) {
+        loop:       while (true) {
+            switch (c = in.read()) {
                 case -1:
                 case '\n':
                     break loop;
                 case '\r':
                     int c2 = in.read();
-                    if ((c2 != '\n') && (c2 != -1) ) {
-                        in.unread(c2);
-                    }
+                    if ((c2 != '\n') && (c2 != -1)) in.unread(c2);
                     break loop;
                 default:
                     if (--room < 0) {
@@ -37,29 +35,32 @@ public class StreamTool {
                         buf = new char[offset + 128];
                         room = buf.length - offset - 1;
                         System.arraycopy(lineBuffer, 0, buf, 0, offset);
+
                     }
                     buf[offset++] = (char) c;
                     break;
             }
         }
-
-        if ((c == -1) && (offset == 0)) {
-            return null;
-        }
+        if ((c == -1) && (offset == 0)) return null;
         return String.copyValueOf(buf, 0, offset);
     }
 
-    public static byte[] readStram(InputStream inputStream) throws Exception {
-        ByteArrayOutputStream outsteam = new ByteArrayOutputStream();
+    /**
+     * 读取流
+     * @param inStream
+     * @return 字节数组
+     * @throws Exception
+     */
+    public static byte[] readStream(InputStream inStream) throws Exception{
+        ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int len = -1;
-        while ((len = inputStream.read(buffer)) != -1) {
-            outsteam.write(buffer, 0, len);
+        while( (len=inStream.read(buffer)) != -1){
+            outSteam.write(buffer, 0, len);
         }
-        outsteam.close();
-        inputStream.close();
-        return outsteam.toByteArray();
-
+        outSteam.close();
+        inStream.close();
+        return outSteam.toByteArray();
     }
 
 }
